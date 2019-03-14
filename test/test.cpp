d8051d241e547bf1ae86fd084381f169548553d6
Aliaksei <alexx333by@protonmail.com>
2019-03-14 11:25:24 +0300

    Isosceles triangle test added

--- test/test.cpp
+++ test/test.cpp
@@ -21,3 +21,32 @@ BOOST_AUTO_TEST_CASE(ordinary_test)
     BOOST_TEST(!triangle.IsEquilateral());
     BOOST_TEST(!triangle.IsIsosceles());
 }
+
+BOOST_AUTO_TEST_CASE(isosceles_test)
+{
+    std::vector<double> sides;
+    Triangle triangle;
+
+    sides.push_back(2);
+    sides.push_back(2);
+    sides.push_back(3);
+
+    BOOST_TEST_REQUIRE(triangle.Initialize(sides) == SUCCESS);
+    BOOST_TEST(triangle.IsIsosceles());
+
+    sides.clear();
+    sides.push_back(2);
+    sides.push_back(3);
+    sides.push_back(2);
+
+    BOOST_TEST_REQUIRE(triangle.Initialize(sides) == SUCCESS);
+    BOOST_TEST(triangle.IsIsosceles());
+
+    sides.clear();
+    sides.push_back(3);
+    sides.push_back(2);
+    sides.push_back(2);
+
+    BOOST_TEST_REQUIRE(triangle.Initialize(sides) == SUCCESS);
+    BOOST_TEST(triangle.IsIsosceles());
+}
