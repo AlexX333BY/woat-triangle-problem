1a5253bbfa3cad9f0320786a568fa582198e66ab
Aliaksei <alexx333by@protonmail.com>
2019-03-14 11:22:37 +0300

    Ordinary triangle test added

--- test/test.cpp
+++ test/test.cpp
@@ -6,6 +6,18 @@ BOOST_AUTO_TEST_CASE(equilateral_test)
 {
     std::vector<double> sides(3, 3);
     Triangle triangle;
-    BOOST_TEST(triangle.Initialize(sides) == SUCCESS);
+    BOOST_TEST_REQUIRE(triangle.Initialize(sides) == SUCCESS);
     BOOST_TEST(triangle.IsEquilateral());
+}
+
+BOOST_AUTO_TEST_CASE(ordinary_test)
+{
+    std::vector<double> sides;
+    sides.push_back(2);
+    sides.push_back(3);
+    sides.push_back(4);
+    Triangle triangle;
+    BOOST_TEST_REQUIRE(triangle.Initialize(sides) == SUCCESS);
+    BOOST_TEST(!triangle.IsEquilateral());
+    BOOST_TEST(!triangle.IsIsosceles());
 }
