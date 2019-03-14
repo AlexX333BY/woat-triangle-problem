5ff2ca93ffe2d4cfabcb682e6340568431a645af
Aliaksei <alexx333by@protonmail.com>
2019-03-14 11:36:28 +0300

    Removed sides clearing

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,7 @@
 #define BOOST_TEST_MODULE woat_triangle_problem_test
 #include <boost/test/unit_test.hpp>
 #include "../src/Triangle.h"
+#include <float.h>
 
 BOOST_AUTO_TEST_CASE(equilateral_test)
 {
@@ -24,28 +25,26 @@ BOOST_AUTO_TEST_CASE(ordinary_test)
 
 BOOST_AUTO_TEST_CASE(isosceles_test)
 {
-    std::vector<double> sides;
+    std::vector<double> sides(3);
     Triangle triangle;
 
-    sides.push_back(2);
-    sides.push_back(2);
-    sides.push_back(3);
+    sides[0] = 2;
+    sides[1] = 2;
+    sides[2] = 3;
 
     BOOST_TEST_REQUIRE(triangle.Initialize(sides) == SUCCESS);
     BOOST_TEST(triangle.IsIsosceles());
 
-    sides.clear();
-    sides.push_back(2);
-    sides.push_back(3);
-    sides.push_back(2);
+    sides[0] = 2;
+    sides[1] = 3;
+    sides[2] = 2;
 
     BOOST_TEST_REQUIRE(triangle.Initialize(sides) == SUCCESS);
     BOOST_TEST(triangle.IsIsosceles());
 
-    sides.clear();
-    sides.push_back(3);
-    sides.push_back(2);
-    sides.push_back(2);
+    sides[0] = 3;
+    sides[1] = 2;
+    sides[2] = 2;
 
     BOOST_TEST_REQUIRE(triangle.Initialize(sides) == SUCCESS);
     BOOST_TEST(triangle.IsIsosceles());
